976934ff044111eb86ebe6b4a573ebe7e59053f3
anshay-zerozilla <anshay.zerozilla@gmail.com>
2024-01-20 13:07:02 +0530

    Auth viewmodel and auth service refactored

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,19 +1,70 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:rarecrew/models/user_model.dart';
 
-class AuthService {
+class AuthService extends ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<User?> signInWithEmailPassword(String email, String password) async {
+  AppUser? _appUser;
+
+  AppUser? get appUser => _appUser;
+
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
     try {
       UserCredential result = await _auth.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
-      User? user = result.user;
-      return user;
+
+      String defaultName = email.split('@').first;
+      _appUser = AppUser(
+        uid: result.user!.uid,
+        email: result.user!.email!,
+        name: defaultName, // Replace with actual user name
+      );
+
+      notifyListeners();
     } catch (e) {
       print('Error signing in: $e');
-      return null;
+      throw e;
     }
+  }
+
+  Future<void> registerWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      String defaultName = email.split('@').first;
+      _appUser = AppUser(
+        uid: result.user!.uid,
+        email: result.user!.email!,
+        name: defaultName, // Replace with actual user name
+      );
+
+      // Create a user document in the 'live/users' collection
+      DocumentReference userDocRef =
+      _firestore.collection('live').doc('users').collection('users').doc(_appUser!.uid);
+      await userDocRef.set({
+        'email': _appUser!.email,
+        'name': _appUser!.name,
+        'uid': _appUser!.uid,
+      });
+
+      notifyListeners();
+    } catch (e) {
+      print('Error registering: $e');
+      throw e;
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+    _appUser = null;
+    notifyListeners();
   }
 }
--- lib/viewmodels/auth_viewmodel.dart
+++ lib/viewmodels/auth_viewmodel.dart
@@ -1,69 +1,35 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:rarecrew/models/user_model.dart';
+import 'package:provider/provider.dart';
+import 'package:rarecrew/services/auth_service.dart';
+
+import '../models/user_model.dart';
 
 class AuthViewModel extends ChangeNotifier {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final AuthService _authService = AuthService();
 
-  AppUser? _appUser;
-
-  AppUser? get appUser => _appUser;
+  AppUser? get appUser => _authService.appUser;
 
   Future<void> signInWithEmailAndPassword(String email, String password) async {
     try {
-      UserCredential result = await _auth.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-
-      String defaultName = email.split('@').first;
-      _appUser = AppUser(
-        uid: result.user!.uid,
-        email: result.user!.email!,
-        name: defaultName, // Replace with actual user name
-      );
-
-      notifyListeners();
+      await _authService.signInWithEmailAndPassword(email, password);
     } catch (e) {
-      print('Error signing in: $e');
       throw e;
     }
   }
 
   Future<void> registerWithEmailAndPassword(String email, String password) async {
     try {
-      UserCredential result = await _auth.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-
-      String defaultName = email.split('@').first;
-      _appUser = AppUser(
-        uid: result.user!.uid,
-        email: result.user!.email!,
-        name: defaultName, // Replace with actual user name
-      );
-      // Create a user document in the 'live/users' collection
-      DocumentReference userDocRef = _firestore.collection('live').doc('users').collection("users").doc(_appUser!.uid);
-      await userDocRef.set({
-        'email': _appUser!.email,
-        'name': _appUser!.name,
-        "uid": _appUser!.uid,
-      });
-
-
-      notifyListeners();
+      await _authService.registerWithEmailAndPassword(email, password);
     } catch (e) {
-      print('Error registering: $e');
       throw e;
     }
   }
 
   Future<void> signOut() async {
-    await _auth.signOut();
-    _appUser = null;
-    notifyListeners();
+    try {
+      await _authService.signOut();
+    } catch (e) {
+      throw e;
+    }
   }
 }
